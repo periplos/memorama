b90112a8b4408ba898633fd221a1705dd2e7291e
periplos <equipoperiplos@gmail.com>
2024-11-17 22:31:02 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const totalCards = 12;
+const totalCards = 10;
 const availableCards = ['A', 'K', 'Q', 'J','C','D'];
 const availableimg = ["images/Comal.png","images/Botas.png","images/Huacal.png","images/Metate.png","images/Olla.png","images/Prensa.png",];
 let cards = [];
@@ -6,7 +6,10 @@ let selectedCards = [];
 let valuesUsed = [];
 let currentMove = 0;
 let currentAttempts = 0;
-
+let currentLives = 0;
+let aciertos = 0;
+let timer=30;
+let nivel = 0;
 let cardTemplate = '<div class="card"><div class="back"></div><div class="face"></div></div>';
 
 function activate(e) {
@@ -24,17 +27,30 @@ function activate(e) {
             if (selectedCards[0].querySelectorAll('.face')[0].innerHTML == selectedCards[1].querySelectorAll('.face')[0].innerHTML) {
                selectedCards = [];
                currentMove = 0;
+               aciertos++;
+               
+               
             }
             else {
                setTimeout(() => {
                   selectedCards[0].classList.remove('active');
                   selectedCards[1].classList.remove('active');
                   selectedCards = [];
+                  currentLives --;
+                  document.querySelector('#lives').innerHTML = currentLives + ' vidas';
                   currentMove = 0;
+                  if(currentLives==0){
+                     window.alert("Se acabaron tus vidas :(");
+                     restart();
+                  }
                }, 600);
             }
          }
       }
+   }
+   if(aciertos==5){
+      window.alert("Felicidades has ganado");
+      restart();
    }
 }
 
@@ -57,7 +73,24 @@ function getFaceValue(value) {
    return rtn;
 }
 
+function started(nivel){
 
+const containerButton = document.getElementById("buttonContainer");
+containerButton.remove();
+
+if(nivel==1){
+timer=50;
+currentLives=4
+}else if(nivel==2){
+   timer=40;
+   currentLives=3
+}else if(nivel==3){
+   timer=30;
+   currentLives=2
+}
+
+document.querySelector('#lives').innerHTML = currentLives + ' vidas';
+contarTiempo();
 
 for (let i=0; i < totalCards; i++) {
    let div = document.createElement('div');
@@ -65,9 +98,42 @@ for (let i=0; i < totalCards; i++) {
    cards.push(div);
    document.querySelector('#game').append(cards[i]);
    randomValue();
-   const img = document.createElement('img');
-   img.src = valuesUsed[i];
-   cards[i].querySelectorAll('.face')[0].innerHTML = getFaceValue(valuesUsed[i]);
-   cards[i].querySelectorAll('.face')[0].appendChild(img);
+   
+
+   var myImage = new Image();
+   myImage.src = getFaceValue(valuesUsed[i]);
+   console.log(myImage);
+
+   
+   
+
+   cards[i].querySelectorAll('.face')[0].appendChild(myImage);
    cards[i].querySelectorAll('.card')[0].addEventListener('click', activate);
+}
+}
+
+function contarTiempo(){
+   setInterval(()=>{
+      timer --;
+      document.querySelector('#time').innerHTML = timer + 'segundos';
+      if(timer==0){
+         window.alert("El tiempo se ha terminado");
+         restart();
+      }
+   },1000);
+   
+}
+
+
+function availableButton(){
+   const mainbutton = document.getElementById("mainButton");
+   mainbutton.remove();
+   let buttonContainer = document.getElementById("buttonContainer").innerHTML +=
+   '<button onclick="started(1)" class="mainButton" >Facil</button><button onclick="started(2)" class="mainButton">Intermedio</button><button onclick="started(3)" class="mainButton">Difícil</button>';
+
+}
+
+
+function restart(){
+   setTimeout(location.reload(),1500);
 }
